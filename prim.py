b8f00da6c98fcdf1c138b1ddf77e2b03e57fc5fa
11jolek11 <dabrowskiandrzej094@gmail.com>
2022-06-04 20:27:36 +0200

    kruskal.py added

--- prim.py
+++ prim.py
@@ -1,56 +1,62 @@
 import sys
 
-import numpy as np
-
-def adj_list_to_matrix(adj_list):
-    n = len(adj_list)
-    adj_matrix = np.nan * np.ones((n,n))
-    np.fill_diagonal(adj_matrix,0)
-
-    for i in range(n):
-        for j, w in adj_list[i]:
-            adj_matrix[i,j] = w
-    return adj_matrix
-
 class dane:
-    def __init__(self):
-        self.koszt = sys.maxsize
-        self.poprzednik = -1
+	def __init__(self):
+		self.koszt = sys.maxsize
+		self.poprzednik = -1
 
 def AlgorytmPrima(macierz, s):
-  n = len(macierz)
-  wynik = [dane() for x in range(n)];
-  wynik[s].koszt = 0
-  kolejka = list(range(n))
-  while (len(kolejka) != 0):
-    u = -1
-    for q in kolejka:
-      if (wynik[q].koszt != sys.maxsize and
-        (u == -1 or wynik[q].koszt < wynik[u].koszt)):
-        u = q;
-    for j in range(n):
-      try:
-        kolejka.index(j)
-        if (macierz[u][j] == 0):
-          continue
-      except ValueError:
-        continue
-      if (wynik[j].koszt > macierz[u][j]):
-        wynik[j].koszt = macierz[u][j]
-        wynik[j].poprzednik = u
-    kolejka.remove(u)
-    print(str(u))
-  return wynik
+	n = len(macierz)
+	wynik = [dane() for x in range(n)]
+	wynik[s].koszt = 0
+	kolejka = list(range(n))
+	while len(kolejka) != 0:
+		u = -1
+		for q in kolejka:
+			if wynik[q].koszt != sys.maxsize and (u == -1 or wynik[q].koszt < wynik[u].koszt):
+				u = q
+		for j in range(n):
+			try:
+				kolejka.index(j)
+				if macierz[u][j] == 0:
+					continue
+			except ValueError:
+				continue
+			if wynik[j].koszt > macierz[u][j]:
+				wynik[j].koszt = macierz[u][j]
+				wynik[j].poprzednik = u
+		kolejka.remove(u)
+		print(str(u))
+	return wynik
+
 
 if __name__ == "__main__":
-	n = int(input("Ile wierzchołków ma graf?\n n = "))
-	s = int(input("Od którego wierzchołka zacząć?\n s = "))
-	print("Podaj input_matrix:")
-	macierz = [[int(x) for x in input().split()] for y in range(n)]
-	wynik = AlgorytmPrima(macierz, s)
+	macierz = [[0, 6, 3, 4, 1],
+[6, 0, 1, 0, 0],
+[3, 1, 0, 2, 1],
+[4, 0, 2, 0, 3],
+[1, 0, 1, 3, 0]]
+
+	wynik = AlgorytmPrima(macierz, 1)
 	suma = 0
 	print("ID\tPoprzednik")
-	for i in range(n):
-	  print(str(i) + "\t" + str(wynik[i].poprzednik))
-	  suma += wynik[i].koszt
-	print("Łączny koszt wynosi: " + str(suma))
+	for i in range(len(macierz)):
+		print(str(i) + "\t" + str(wynik[i].poprzednik))
+		suma += wynik[i].koszt
+	print("ĹÄczny koszt wynosi: " + str(suma))
+
+
+"""
+[[0, 6, 3, 4, 1],
+[6, 0, 1, 0, 0],
+[3, 1, 0, 2, 1],
+[4, 0, 2, 0, 3],
+[1, 0, 1, 3, 0]]
+
+
+0, 6, 3, 4, 1
+6, 0, 1, 0, 0
+3, 1, 0, 2, 1
+4, 0, 2, 0, 3
+1, 0, 1, 3, 0
+"""
