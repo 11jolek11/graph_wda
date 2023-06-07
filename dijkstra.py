6521e511c0b920e561519566cc69c82f8d0b1629
11jolek11 <dabrowskiandrzej094@gmail.com>
2023-06-07 12:49:09 +0200

    dijkstra and kruskal

--- dijkstra.py
+++ dijkstra.py
@@ -1,117 +1,51 @@
-import sys
-# https://graphonline.ru/en/
+from collections import namedtuple, deque
+from pprint import pprint as pp
+ 
+ 
+inf = float('inf')
+Edge = namedtuple('Edge', ['start', 'end', 'cost'])
 
+class Graph():
+    def __init__(self, edges):
+        self.edges = [Edge(*edge) for edge in edges]
+        # print(dir(self.edges[0]))
+        self.vertices = {e.start for e in self.edges} | {e.end for e in self.edges}
+ 
+    def dijkstra(self, source, dest):
+        # TODO: zmien assert na if'a
+        assert source in self.vertices
+        dist = {vertex: inf for vertex in self.vertices}
+        previous = {vertex: None for vertex in self.vertices}
+        dist[source] = 0
+        q = self.vertices.copy()
+        neighbours = {vertex: set() for vertex in self.vertices}
+        for start, end, cost in self.edges:
+            neighbours[start].add((end, cost))
+            neighbours[end].add((start, cost))
 
-"""
-Algorytm Dijkstry przy użyciu macierzy (w materiałach źródłowych nie było
-opisu listy z wagami, więc użyłem macierzy)
-"""
-
-
-class Helper(object):
-	def __init__(self, v, p, d):
-		self.visiteddd = v
-		self.predecessor = p
-		self.dist = d
-
-
-def print_out(line, d):
-	txt = str(line) + "\t"
-	if not d.visiteddd:
-		txt += "not visited"
-	else:
-		if d.predecessor == -1:
-			txt += "None"
-		# else:
-		# 	txt += str(d.predecessor)
-		txt += "\t" + str(d.dist)
-	print(txt)
-
-
-def search_for_minimal(matrix):
-	minimal = -1
-	minimal_dist = 100000
-	for i in range(0, len(matrix)):
-		if (not matrix[i].visiteddd) and matrix[i].dist < minimal_dist:
-			minimal = i
-			minimal_dist = matrix[i].dist
-	return minimal
-
-
-def dijkstra(input_matrix, starting_pont):
-	temp = []
-	for p in range(0, len(input_matrix)):
-		temp.append(Helper(False, -1, 100000))
-	temp[starting_pont].dist = 0
-	u = starting_pont
-	while u != -1:
-		temp[u].visiteddd = True
-		for p in range(0, len(input_matrix)):
-			if input_matrix[u][p] > 0 and temp[u].dist + input_matrix[u][p] < temp[p].dist:
-				temp[p].dist = temp[u].dist + input_matrix[u][p]
-				temp[p].predecessor = u
-		u = search_for_minimal(temp)
-	return temp
-
-
-if __name__ == "__main__":
-	# tablica = [[0, 6, 3, 4, 1],
-	# [6, 0, 1, 0, 0],
-	# [3, 1, 0, 2, 1],
-	# [4, 0, 2, 0, 3],
-	# [1, 0, 1, 3, 0]]
-
-# 	tablica = [[0, 6, 3, 4, 1, 0, ],
-# 	[6, 0, 1, 0, 0, 3, ],
-# 	[3, 1, 0, 2, 1, 0, ],
-# 	[4, 0, 2, 0, 3, 0, ],
-# 	[1, 0, 1, 3, 0, 0, ],
-# 	[0, 3, 0, 0, 0, 0, ]]
-
-	# http://graphonline.ru/en/?graph=XAGNhhbcLDgFUMcc
-	# tablica = [[0, 6, 3, 4, 1, 0, 0, 0,],
-	# [6, 0, 1, 0, 0, 3, 0, 0,],
-	# [3, 1, 0, 2, 1, 0, 0, 0,],
-	# [4, 0, 2, 0, 3, 0, 0, 0,],
-	# [1, 0, 1, 3, 0, 0, 0, 0,],
-	# [0, 3, 0, 0, 0, 0, 0, 0,],
-	# [0, 0, 0, 0, 0, 0, 0, 1,],
-	# [0, 0, 0, 0, 0, 0, 1, 0,]]
-
-	# http://graphonline.ru/en/?graph=XAGNhhbcLDgFUMcc
-	tablica = [[0, 12, 12, 12, 1, 0, 0, 0,],
-[12, 0, 1, 0, 0, 3, 0, 0,],
-[12, 1, 0, 2, 12, 0, 0, 0,],
-[12, 0, 2, 0, 3, 0, 0, 0,],
-[1, 0, 12, 3, 0, 0, 0, 0,],
-[0, 3, 0, 0, 0, 0, 0, 0,],
-[0, 0, 0, 0, 0, 0, 0, 1,],
-[0, 0, 0, 0, 0, 0, 1, 0,]]
-
-	dij = dijkstra(tablica, 0)
-	print("Node Direction Dist")
-	for i in range(0, len(tablica)):
-		# print(dij[i])
-		print_out(i, dij[i])
-
-"""
-
-Numeracja wierzchołków zaczyna się od zera (patrz: graf_diksta.png)
-http://graphonline.ru/en/?graph=MMqnxjyMrgDKeDDq
-
-0 6 3 4 1
-6 0 1 0 0
-3 1 0 2 1
-4 0 2 0 3
-1 0 1 3 0
-"""
-
-"""
-[[0, 6, 3, 4, 1, 0,],
-[6, 0, 1, 0, 0, 3,],
-[3, 1, 0, 2, 1, 0,],
-[4, 0, 2, 0, 3, 0,],
-[1, 0, 1, 3, 0, 0,],
-[0, 3, 0, 0, 0, 0,]]
-
-"""
+        #pp(neighbours)
+ 
+        while q:
+            # pp(q)
+            u = min(q, key=lambda vertex: dist[vertex])
+            q.remove(u)
+            if dist[u] == inf or u == dest:
+                break
+            for v, cost in neighbours[u]:
+                alt = dist[u] + cost
+                if alt < dist[v]:                                  # Relax (u,v,a)
+                    dist[v] = alt
+                    previous[v] = u
+        #pp(previous)
+        s, u = deque(), dest
+        while previous[u]:
+            s.appendleft(u)
+            u = previous[u]
+        s.appendleft(u)
+        return s
+ 
+ 
+graph = Graph([("a", "b", 7),  ("a", "c", 9),  ("a", "f", 14), ("b", "c", 10),
+               ("b", "d", 15), ("c", "d", 11), ("c", "f", 2),  ("d", "e", 6),
+               ("e", "f", 9)])
+pp(graph.dijkstra("a", "e"))
